87fc3584a6f091cd71d90573348c02ce46bb8a57
GaboLindo <129307064+GaboLindo@users.noreply.github.com>
2023-09-11 10:49:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+escrever = msg=> alert(msg);
+soma = (a,b) => a + b;
+sub= (a,b) => a - b;
+mult = (a,b) => a * b;
+raiz = a => Math.sqrt(a);
+equacao2grau =(a,b,c) =>{
+
+   let delta = sub(mult(b,b),mult(4,mult(a,c)));
+   if(delta< 0 ) return "nao possui raiz real.";
+   if(delta == 0 ) return "X1 = X2 =." + div(-b,mult(2,a));
+   return "X1 = " + div (soma (-b,raiz(delta)),mult(2,a)) + " X2 ="
+     + div (sub (-b,raiz(delta)),mult(2,a))
+   
+     }
+     escrever(equacao2grau(3,2,1))
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
